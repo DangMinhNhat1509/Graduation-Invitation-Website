2c845f43ea6eb06d31a8870c8b5075d25266b858
Nhat Minh Dang <dangminhnhat10988@gmail.com>
2025-11-20 18:49:59 +0700

    Fix firebase env for deploy

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,23 +1,14 @@
-// lib/firebase.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config từ env
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FB_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FB_AUTH!,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT!,
-  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_SENDER!,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT!,
+  apiKey: process.env.NEXT_PUBLIC_FB_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTH,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECT,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_SENDER,
+  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 };
 
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
-
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 export const db = getFirestore(app);
